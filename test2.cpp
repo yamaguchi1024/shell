9df1b9400b0394dba7f79bcc816b45b8bc4b55f7
cookie-s <cookie-s@users.noreply.github.com>
2016-10-14 12:39:24 +0900

    add test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,6 +1,5 @@
-#include<cstdio>
+#include<stdio.h>
 #include<iostream>
-#include<vector>
 #include<unistd.h>
 #include<stdlib.h>
 #include<sys/types.h>
@@ -10,28 +9,9 @@
 #include<signal.h>
 #include<fcntl.h>
 
-std::string path(std::string opr){
-    char env[100];
-    char *tmp=getenv("PATH");
-    strcpy(env,tmp);
-    char *tp;
-    tp=strtok(env,":");
-    std::string paths[10];
-    int i=0;
-    while(tp!=NULL){
-        paths[i]=std::string(tp);
-        i++;
-        tp=strtok(NULL,":");
-    }
-    std::string rtn=opr;
-    for(int j=0;j<i;j++){
-        if(!access((paths[j]+"/"+opr).c_str(),X_OK)){
-            rtn=paths[j]+"/"+opr;
-            break;
-        }
-    }
-    return rtn;
-}
+#include "util.h"
+#include "ringlist.h"
+
 int execute3(std::string oprand[],int size, int in, int out){
     int fork_result = fork();
     switch(fork_result) {
@@ -58,25 +38,25 @@ int execute3(std::string oprand[],int size, int in, int out){
 
 }
 
-void execute2(std::vector<int>& childpid, std::string oprand[],int size,int in, int out){
+void execute2(ringlist *childpid, std::string oprand[],int size,int in, int out){
     //リダイレクトとパイプライン処理
     int q=0;
     int last=size;
     for(q=0;q<size;q++){
         if(oprand[q]=="<"){
-            last = std::min(last, q);
+            last = min(last, q);
             if(in != 0) close(in);
             in = open(oprand[q+1].c_str(),O_RDONLY);
             q++;
         }
         else if(oprand[q]==">"){
-            last = std::min(last, q);
+            last = min(last, q);
             if(out!= 1) close(out);
             out = open(oprand[q+1].c_str(),O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWGRP | S_IWUSR);
             q++;
         }
         else if(oprand[q]=="|"){
-            last = std::min(last, q);
+            last = min(last, q);
             int pipefd[2];
             pipe2(pipefd,O_CLOEXEC);
             execute2(childpid,oprand+q+1,size-q-1,pipefd[0],1);
@@ -91,33 +71,30 @@ void execute2(std::vector<int>& childpid, std::string oprand[],int size,int in, 
     if(out!= 1) close(out);
 
     if(cpid != -1)
-        childpid.push_back(cpid);
-    //std::cout<< "created pid" <<  getpid() << std::endl;
-    //printf("%d\n",execl("/bin/ls","/bin/ls",NULL));
-
-    //std::cout <<"Child pid" << fork_result << std::endl;
+        ringlist_add(childpid, cpid);
 }
 
 void execute(std::string oprand[],int size) {
-    std::vector<int> children;
-    execute2(children,oprand,size,0,1);
+    ringlist *childpid = ringlist_init();
+    execute2(childpid,oprand,size,0,1);
 
-    while(children.size())
-        for(std::vector<int>::iterator it = children.begin(); it != children.end(); ) {
-            int status;
-            if(!waitpid(*it,&status,WUNTRACED | WNOHANG)) {
-                it++;
-                continue;
-            }
-            children.erase(it);
-
-            printf("child process done.\n");
-            if(WIFEXITED(status)){
-                printf("child process exit status=%d\n",WEXITSTATUS(status));
-            }else{
-                printf("exit abnormally\n");
-            }
+    ringlist *it;
+    for(it = childpid; it->next; ) {
+        int status;
+        if(!waitpid(it->num,&status,WUNTRACED | WNOHANG)) {
+            it = it->next;
+            continue;
         }
+        it = ringlist_erase(it);
+
+        printf("child process done.\n");
+        if(WIFEXITED(status)){
+            printf("child process exit status=%d\n",WEXITSTATUS(status));
+        }else{
+            printf("exit abnormally\n");
+        }
+    }
+    free(it);
     printf("all children exited\n");
     return;
 
@@ -125,7 +102,7 @@ void execute(std::string oprand[],int size) {
 void SigHandler(int p_signame){
     signal(SIGINT,SigHandler);	
     printf("\n\033[%dm>>\033[0m",31);
-    std::cout << std::flush ;
+    fflush(stdout);
     return;
 }	
 
